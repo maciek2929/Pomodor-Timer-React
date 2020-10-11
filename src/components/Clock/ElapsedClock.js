11eaed23a752e4e926006b7be0a9caccfe21e249
maciek2929 <maciej22.opozda@gmail.com>
2020-10-11 18:56:34 +0200

    new changes

---
+++ src/components/Clock/ElapsedClock.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ElapsedClock = () => {
+    return (
+        <div>
+            <h2>Helllo</h2>
+            
+        </div>
+    )
+}
+
+export default ElapsedClock
