714dacab9fa8541e786f986c07219b68b81961d9
maciek2929 <maciej22.opozda@gmail.com>
2020-10-29 15:05:19 +0100

    refactoring State in Timebox List

--- src/components/TimeboxList/TimeboxList.jsx
+++ src/components/TimeboxList/TimeboxList.jsx
@@ -5,20 +5,12 @@ import TimeboxCreator from "../TimeboxCreator/TimeboxCreator";
 import TimeboxElement from "../TimeboxElement/TimeboxElement";
 
 const TimeboxList = () => {
-  const [Statetimebox, setStateTimebox] = useState({
-    timeboxes: [
-      {
-        id: uuidv4(),
-        title: "proboje z lista dynamicznie",
-        totalTimeInMinutes: 15,
-      },
-    ],
-  });
+ const [timebox, setTimebox] = useState([])
 
   const addTimebox = (timebox) => {
-    setStateTimebox((prev) => {
-      const timeboxes = [...prev.timeboxes, timebox];
-      return { timeboxes };
+    setTimebox((prev) => {
+      const timeboxes = [...prev, timebox];
+      return  timeboxes ;
     });
   };
 
@@ -26,24 +18,24 @@ const TimeboxList = () => {
     addTimebox(createdTimebox);
   };
 
-  const removeTimebox = (indexToRemove) => {
-    setStateTimebox((prev) => {
-      const timeboxes = prev.timeboxes.filter(
-        (timebox, index) => index !== indexToRemove
+  const removeTimebox = (idToRemove) => {
+    setTimebox((prev) => {
+      const timeboxes = prev.filter(
+        (timebox) => timebox.id !== idToRemove
       );
-      return { timeboxes };
+      return timeboxes ;
     });
   };
 
   return (
     <div>
       <TimeboxCreator onCreate={handleCreate} />
-      {Statetimebox.timeboxes.map((timebox, index) => (
+      {timebox.map((timebox, index) => (
         <TimeboxElement
           key={timebox.id}
           title={timebox.title}
           totalTimeInMinutes={timebox.totalTimeInMinutes}
-          onDelete={() => removeTimebox(index)}
+          onDelete={() => removeTimebox(timebox.id)}
           onEdit={() => console.log("robie edycje", timebox.id)}
         />
       ))}
